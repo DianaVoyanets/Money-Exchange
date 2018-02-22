5ac917a5fca5137409e9f7ab8e4218373f8eccd3
d.voyanets <diana.voyanets@gmail.com>
2018-02-22 22:04:40 +0300

    Done!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency < 0) return {};
+    
+    if (currency > 10000) {
+        return { 
+            error: 'You are rich, my friend! We don\'t have so much coins for exchange' 
+        };
+    }
+
+    var coins = [1, 5, 10, 25, 50],
+        signs = ['P', 'N', 'D', 'Q', 'H'],
+        data = [0].concat(Array(currency).fill(Infinity)),
+        denom = Array(currency + 1).fill(0),
+        result = {};
+
+    for (var i = 1; i < currency + 1; i++) 
+        for (var j = 1; j < coins.length + 1; j++) 
+            if (coins[j] <= i && data[i - coins[j]] < data[i]) {
+                data[i] = data[i - coins[j]] + 1;
+                denom[i] = j;
+            }
+
+    while (currency) {
+        var sign = signs[denom[currency]];
+
+        if (sign in result) {
+            result[sign]++;
+        } else {
+            result[sign] = 1;
+        }
+
+        currency -= coins[denom[currency]];
+    }
+
+    return result;
 }
